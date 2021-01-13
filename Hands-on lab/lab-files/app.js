24ef2f45a697658f4765acbf9dfa5c1be61702e6
Ubuntu <a11027@alunos.ipca.pt>
2021-01-13 12:25:48 +0000

    Add docker configuration

--- Hands-on lab/lab-files/app.js
+++ Hands-on lab/lab-files/app.js
@@ -13,7 +13,7 @@ var userSession = require('./routes/session');
 
 var app = express();
 
-var databaseUrl = 'mongodb://localhost:27017/best-for-you-organics';
+var databaseUrl = 'mongodb://best-for-you-db-helder:CPP5LHrCg1s6PoVqUHg6GiakRfie2hn7Ua5tgstTsEnUi9Ue6GBvj6qYObOfIQlR3xf0jdyeKxSOYUfGNWgl6A==@best-for-you-db-helder.mongo.cosmos.azure.com:10255/best-for-you-organics?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@best-for-you-db-helder@';
 
 var mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
